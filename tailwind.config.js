7ac9e8fb75825d94ec3daff710e3d178efc77754
elturanshucai <96484306+elturanshucai@users.noreply.github.com>
2022-08-04 22:35:08 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      spacing: {
+        '15': '3.75rem'
+      }
+    },
+  },
+  plugins: [],
+}
