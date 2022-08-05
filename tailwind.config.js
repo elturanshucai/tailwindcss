50b542e6f4623b88a1969671206e67b0350b94bb
elturanshucai <96484306+elturanshucai@users.noreply.github.com>
2022-08-05 16:39:43 +0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
+  darkMode: 'class',
   theme: {
     extend: {
+      colors:{
+        prototurk : '#32445a',
+        twitter: '#1da1f2'
+      },
       spacing: {
         '15': '3.75rem'
-      }
+      },
+      zIndex: {
+        '-1':'-1'
+      } 
     },
   },
   plugins: [],
